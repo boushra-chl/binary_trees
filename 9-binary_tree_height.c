9c4fa3b0ed283ce8a06d1475d17ea9a8e57ec89f
boushra-chl <bchilouch1998@gmail.com>
2024-06-09 20:06:56 +0100

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,8 +16,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (left_height > right_height ? left_height : right_height);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
+	if (right_height > left_height)
+		return (right_height);
+	else
+		return (left_height);
 }
