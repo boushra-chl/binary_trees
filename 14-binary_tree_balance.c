ce69a1a4724be81851b0175a305223c1068d13c7
coushra-chl <bchilouch1998@gmail.com>
2024-06-11 13:30:06 +0100

    14 betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,10 +19,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	left_height = binary_tree_height(tree->left) + 1;
 	right_height = binary_tree_height(tree->right) + 1;
-        if (right_height > left_height)
-                return (right_height);
-        else
-                return (left_height);
+	if (right_height > left_height)
+		return (right_height);
+	else
+		return (left_height);
 }
 
 /**
